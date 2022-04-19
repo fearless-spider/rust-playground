06ca54074c61f01331a5be42df9cc50cc444a31e
Przemyslaw Pajak <przemyslaw.pajak@creative-works.us>
2022-04-19 12:48:06 +0200

    Hello Rust

---
+++ hello-rust/target/debug/deps/hello_rust-fc28144dfdb55c8e.d
@@ -0,0 +1,5 @@
+/Users/fearless/Code/Rust/rust-playground/hello-rust/target/debug/deps/hello_rust-fc28144dfdb55c8e: src/main.rs
+
+/Users/fearless/Code/Rust/rust-playground/hello-rust/target/debug/deps/hello_rust-fc28144dfdb55c8e.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/fearless/Code/Rust/rust-playground/hello-rust/target/debug/hello-rust: /Users/fearless/Code/Rust/rust-playground/hello-rust/src/main.rs
